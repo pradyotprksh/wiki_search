80c1077fb6b967a423eaf008bad5588a40f28523
pradyotMobifyi <75113257+pradyotMobifyi@users.noreply.github.com>
2021-01-06 21:05:29 +0530

    style(View): Search screen
    - Added floating action button

--- lib/src/views/ui/search_screen/search_screen.dart
+++ lib/src/views/ui/search_screen/search_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared/shared.dart';
 
 class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         backgroundColor: Get.theme.backgroundColor,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: ColorsValue.primaryColor,
+          child: const Icon(
+            Icons.history,
+          ),
+        ),
       );
 }
