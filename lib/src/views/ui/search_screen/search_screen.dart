a648492f8024708085a0e26d4d6e9655319ecde3
pradyotMobifyi <75113257+pradyotMobifyi@users.noreply.github.com>
2021-01-06 21:02:25 +0530

    style(View): Search screen
    - Refactored UI for search screen.

--- lib/src/views/ui/search_screen/search_screen.dart
+++ lib/src/views/ui/search_screen/search_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SearchScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Get.theme.backgroundColor,
+      );
 }
